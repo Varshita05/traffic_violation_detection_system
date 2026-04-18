37391a43e7c1d36b5dc804366df806409ea87bff
Varshita05 <varshitaposani.05@gmail.com>
2026-04-18 10:24:56 +0530

    added backend url to socketio and updated frontend api url in violations.js

--- frontend/services/violations.js
+++ frontend/services/violations.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    // baseURL: "http://localhost:5000/api",
+    baseURL: "https://traffic-violation-detection-system-2dtp.onrender.com/api",
 });
 
 // Violations
